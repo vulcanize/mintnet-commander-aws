3c535f2ed193821b28ed472968a269fe05a16ea1
Agnieszka <agnieszka.kramarek@imapp.pl>
2017-03-06 16:10:48 +0100

    added public key to master packer

--- ethermint-testing/packer_configs/packer_salt_master_config.py
+++ ethermint-testing/packer_configs/packer_salt_master_config.py
@@ -2,6 +2,7 @@ packer_salt_ssh_master_config = {
     "variables": {
         "aws_access_key": "",
         "aws_secret_key": "",
+        "master_public_key": "",
         "master_private_key": ""
     },
     'builders': [],
@@ -18,6 +19,7 @@ packer_salt_ssh_master_config = {
                 "sudo apt-get update",
                 "sudo apt-get install -y salt-master salt-ssh",
                 "echo '{{user `master_private_key`}}' >> .ssh/master_key",
+                "echo '{{user `master_public_key`}}' >> .ssh/master_key.pub",
             ]
         },
     ]
